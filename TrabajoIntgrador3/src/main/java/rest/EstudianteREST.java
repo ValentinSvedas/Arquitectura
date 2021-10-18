23b2ca3bf1ea58bd074f719447ba13133ff657ea
Ezequiel Falcon <exzequielfalcon@gmail.com>
2021-10-18 19:39:43 -0300

    Fixed dependencies

--- TrabajoIntgrador3/src/main/java/rest/EstudianteREST.java
+++ TrabajoIntgrador3/src/main/java/rest/EstudianteREST.java
@@ -1,20 +1,26 @@
 package rest;
 
 import entities.Estudiante;
-import jakarta.ws.rs.*;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.core.MediaType;
-import jakarta.ws.rs.core.Response;
 import repository.EstudianteRepository;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Path("/estudiante")
 public class EstudianteREST {
 	
-	@GET @Consumes(MediaType.APPLICATION_JSON)
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response createEstudiante(Estudiante e) {
 		EstudianteRepository.getInstance().add(e);
 		return Response.status(201).entity(e).build();
+	}
+
+	@GET
+	public String checkIfWorks() {
+		return "Works";
 	}
 
 }
