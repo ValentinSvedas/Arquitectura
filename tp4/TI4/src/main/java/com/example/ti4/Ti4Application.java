ab915acf08dee4fc139b42090b4dad6cfb54b6a3
ValentinSvedas <valentinsvedas@gmail.com>
2021-11-02 16:50:53 -0300

    tp4 producto

---
+++ tp4/TI4/src/main/java/com/example/ti4/Ti4Application.java
@@ -0,0 +1,13 @@
+package com.example.ti4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ti4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ti4Application.class, args);
+    }
+
+}
---
+++ tp4/TI4/src/test/java/com/example/ti4/Ti4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ti4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ti4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
